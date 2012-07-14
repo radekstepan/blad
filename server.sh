cf06c8d6e979669e1a846c12e1ded0d1a31f40e4
Radek Stepan <dev@radekstepan.com>
2012-07-14 04:42:41 +0100

    fixes #25

--- server.sh
+++ server.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e
 clear
 ./node_modules/.bin/cake compile
 node server.js
--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+set -e
 clear
 NODE_ENV=test ./node_modules/mocha/bin/mocha --compilers coffee:coffee-script --ui bdd --reporter spec
