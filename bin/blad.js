86a927ca9df58be767cc6ba8729cfcae58472f52
Radek Stepan <dev@radekstepan.com>
2016-02-26 17:40:17 +0100

    parse config and split router

--- bin/blad.js
+++ bin/blad.js
@@ -1,2 +1,2 @@
 #!/usr/bin/env node
-require('../index.js')(process.env);
+require('../index.js')(process.argv.slice(2));
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 "use strict";
 
+let args = require('minimist');
 let path = require('path');
 let _ = require('lodash');
 let as = require('async');
@@ -9,7 +10,21 @@ let content = require('./lib/content.js');
 let server = require('./lib/server.js');
 
 module.exports = (opts) => {
-  opts.PATH = path.resolve(opts.PATH);
+  opts = args(opts, {
+    'alias': {
+      'p': 'port',
+      's': 'source',
+      'c': 'cache'
+    },
+    'default': {
+      // Example site.
+      's': './example/',
+      // Max cache age of 1 day.
+      'c': 60 * 24
+    }
+  });
+
+  opts.source = path.resolve(opts.source);
 
   as.waterfall([
     _.partial(helpers, opts),
